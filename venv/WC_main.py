8c60169f848682210270d582dace37cf4821c894
ewwx <37228820+ewwx@users.noreply.github.com>
2018-03-20 09:02:54 +0200

    I have fixed the program

--- venv/WC_main.py
+++ venv/WC_main.py
@@ -5,13 +5,15 @@ import time
 
 start = time.clock();
 def count_lines():
-    n_of_l = sum(1 for line in open(args.name))
-    return n_of_l
+    f = open(args.name, 'r', encoding='UTF8')
+    n_of_l = re.findall('\n',f.read())
+    f.close()
+    return len(n_of_l)
 
 
 def count_words():
     n_of_w = 0
-    for line in open(args.name):
+    for line in open(args.name,'r',encoding='UTF8'):
         w = re.findall(r'\S+', line)
         n_of_w = n_of_w + len(w)
     return n_of_w
@@ -19,9 +21,13 @@ def count_words():
 
 def count_chars():
     n_of_c = 0
-    for line in open(args.name):
+    for line in open(args.name,'r',encoding='UTF8'):
         c = re.findall('\S', line)
-        n_of_c = n_of_c + len(c)
+        c2 = re.findall('\s', line)
+        c3 = re.findall('\n',line)
+        c4 = re.findall('\r',line)
+
+        n_of_c = n_of_c + len(c) +len(c2) +len(c3) +len(c4)
     return n_of_c
 
 
